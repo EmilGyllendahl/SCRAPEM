315dbc02b5de1da31a9d4b760df2c931bb5dbcc8
Douglas <douglas.torn-persson@edu.futuregames.nu>
2022-11-29 19:38:30 +0100

    A number of changes
    Added a script to scale the car in the main scene.
    Deleted a line in the menu script that kept that scene from changing.
    Added an improvised controller.
    Added Sprites for the car and wheels

---
+++ Assets/blah.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blah : MonoBehaviour
+{
+    public Transform player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player.localScale = new Vector3(1, 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
